efeed459512de5cea4af9d3c32ca54dca9fdea6b
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2022-11-09 19:39:00 -0600

    Readonly propertiy

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface baseModel {
-  id: string;
-  createdAt: Date;
+  readonly id: string; // readonly means that the value can only be set once
+  readonly createdAt: Date; // readonly means that the value can only be set once
   updatedAt: Date;
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -3,5 +3,7 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product): void => {
+  // data.id = '1' // Error: Cannot assign to 'id' because it is a read-only property.
+  // data.createdAt = new Date() // Error: Cannot assign to 'createdAt' because it is a read-only property.
   products.push(data);
 };
